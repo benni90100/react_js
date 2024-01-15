12ba5856e5f0e88db089cd7602428dae5ed4e271
benni90100 <micelibenedetto800@gmail.com>
2024-01-15 22:20:05 +0100

    props-1

---
+++ props-1/src/App.jsx
@@ -0,0 +1,11 @@
+import { Welcome } from "./Welcome";
+
+export function App() {
+    return(
+        <div>
+            <Welcome name={"Benni"}/>
+        </div>
+    )
+}
+//qualora non passassi un valore a name react mi darebbe un errore, mentre se non passo proprio il name
+//l'errore non c'è, 
---
+++ props-1/src/Welcome.jsx
@@ -0,0 +1,6 @@
+export function Welcome({name}) {
+    return (
+    <p>welcome, {name}</p>
+    )
+}
+//per passare un valore di default mi basta passare il props con {name="valore random"}
