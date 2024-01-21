ee861ec8196e9719c7b6c9cf23de5ab8e831e5e2
benni90100 <micelibenedetto800@gmail.com>
2024-01-21 21:02:52 +0100

    uncontrolled-form

---
+++ uncontrolled-form/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { UncontrolledLogin } from './component/UncontrolledLogin'
+
+function App() {
+
+
+  return (
+    <>
+    <UncontrolledLogin/>
+    </>
+  )
+}
+
+export default App
