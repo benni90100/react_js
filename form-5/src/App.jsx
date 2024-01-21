30eff763539470d2d3d46fef1f6341cb87b6c3a6
benni90100 <micelibenedetto800@gmail.com>
2024-01-21 20:21:37 +0100

    form-5

---
+++ form-5/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Login } from "./component/Login";
+
+function App() {
+  function handleLogin(userData) {
+    console.log(userData);
+  }
+
+  return (
+    <>
+      <Login onLogin={handleLogin} />
+    </>
+  );
+}
+
+export default App;
