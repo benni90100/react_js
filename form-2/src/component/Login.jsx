3a008b403aee51ff8210144bd3015ce7454a0af0
benni90100 <micelibenedetto800@gmail.com>
2024-01-21 18:39:36 +0100

    form-2

---
+++ form-2/src/component/Login.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+export function Login() {
+  const [data, setData] = useState({
+    userName: "",
+    password: "",
+    checkBox: false,
+  });
+  function handleDataInput(e) {
+    const name = e.target.name;
+    const value = e.target.value 
+    const checked = e.target.checked
+    const type = e.target.type
+    console.log(value);
+    setData((data) => {
+      return {
+        ...data,
+        [name]: type=="checkbox" ? checked : value
+      };
+    });
+  }
+  return (
+    <div>
+      <input name="userName" value={data.userName} onChange={handleDataInput} />
+      <input
+        type="password"
+        name="password"
+        value={data.password}
+        onChange={handleDataInput}
+      />
+      <input type="checkbox" name="rememberMe" value={data.checkBox} onChange={handleDataInput}/>
+    </div>
+  );
+}
