686de7570a090ca30dacd7437003a0b546e607ea
benni90100 <micelibenedetto800@gmail.com>
2024-02-07 14:50:40 +0100

    2routing

--- first_app1/src/App.jsx
+++ first_app1/src/App.jsx
@@ -1,16 +1,14 @@
-import { Route, Router, Routes } from "react-router-dom";
-import { Hello } from "./component/Hello";
 import { Counter } from "./component/Counter";
-export function App({ name }) {
-  
-  return (
-    <div>
-      <h3>ciao sono {name}</h3>
+import { Welcome } from "./component/Welcome";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-      <Routes>
-        <Route path="/" element={<Hello/>} />
-        <Route path="counter" element={<Counter/>}/>
-      </Routes>
-    </div>
+export function App() {
+  return (
+    <BrowserRouter>
+     <Routes>
+        <Route path='/' element={<Welcome name={"Benni"} />}/>
+        <Route path="/counter" element={<Counter/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
