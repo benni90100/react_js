632d08a3bb381ebbfae3f51d44a805a8bacd8a22
benni90100 <micelibenedetto800@gmail.com>
2024-02-09 12:58:50 +0100

    rout

--- customhook3/src/component/useGithubUsers.jsx
+++ customhook3/src/component/useGithubUsers.jsx
@@ -1,14 +1,20 @@
 import useSWR from 'swr'
+import { useEffect, useState } from "react";
+
 const fetcher = url=> fetch(url).then(res => res.json())
 
-export function useGithubUsers(username) {
+export function useGithubUsers() {
+  const [username, setUsername] = useState(null);
 
   const {data, error, mutate}=useSWR(`https://api.github.com/users/${username}`, fetcher)
-      
-function fetchGitubUser() {
-  mutate()
-}
+  function handleUsername(e) {
+    setUsername(e.target.value);
+  }
+  function getInput(e) {
+    e.preventDefault();
+    mutate()
+    console.log(username)
+  }
 
-  return {data, useFetchUser:fetchGitubUser};
-
+  return [data, getInput, handleUsername];
 }
